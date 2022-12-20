5d621c4e7e20da7bfb614b9fdb525c556807d2df
Kirill Popov <k.popov@hunterhelp.ru>
2022-12-20 17:02:32 +0300

    my nvim config

---
+++ dot_config/nvim/after/plugin/comment.lua
@@ -0,0 +1,9 @@
+local comment = require("Comment")
+require("Comment.api")comment.setup {
+    toggler = {
+        line = '<C-_>'
+    },
+    opleader = {
+        line = '<C-_>'
+    }
+}
---
+++ dot_config/nvim/after/plugin/netrw.lua
@@ -0,0 +1,13 @@
+vim.api.nvim_create_autocmd('filetype', {
+    pattern = 'netrw',
+    desc = 'Better mappings for netrw',
+    callback = function ()
+        local bind = function (lhs, rhs)
+            vim.keymap.set('n', lhs, rhs, {remap = true, buffer = true})
+        end
+        bind('n', '%')
+        bind('r', 'R')
+        bind('l', '<CR>')
+        bind('h', '-')
+    end
+})
