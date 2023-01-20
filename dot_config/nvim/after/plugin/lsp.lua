67bfc87c89ff7bba3fcf35ab48010aae217ed36b
Kirill Popov <k.popov@hunterhelp.ru>
2023-01-20 09:48:38 +0300

    tabs + fromat on save

--- dot_config/nvim/after/plugin/lsp.lua
+++ dot_config/nvim/after/plugin/lsp.lua
@@ -1,4 +1,5 @@
 local lsp = require('lsp-zero')
+require("lsp-format").setup {}
 
 lsp.preset('recommended')
 
@@ -19,9 +20,8 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 lsp.setup_nvim_cmp({
     mapping = cmp_mappings
 })
-
-
-lsp.on_attach(function(client, bufnr)
+local on_attach = function(client, bufnr)
+    require("lsp-format").on_attach(client)
     local opts = { buffer = bufnr, remap = false }
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
@@ -34,7 +34,10 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set("n", "<leader>lrn", function() vim.lsp.buf.rename() end, opts)
     -- vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
     vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format() end, opts)
-end)
+end
+
+lsp.on_attach(on_attach)
+require("lspconfig").gopls.setup { on_attach = on_attach }
 
 lsp.nvim_workspace()
 lsp.setup()
