24295eff288a7a615a0288a5cd7ab218dcd8a099
Kirill Popov <k.popov@hunterhelp.ru>
2023-01-27 16:33:06 +0300

    yep

--- dot_config/nvim/lua/mrpickles/remap.lua
+++ dot_config/nvim/lua/mrpickles/remap.lua
@@ -1,18 +1,28 @@
-
+local map = vim.keymap.set
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+map("n", "<leader>e", vim.cmd.Ex)
 
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+map("v", "J", ":m '>+1<CR>gv=gv")
 
-vim.keymap.set("x", "<leader>p", "\"_dP")
-vim.keymap.set("n", "<leader>d", "\"_dP")
-vim.keymap.set("v", "<leader>d", "\"_dP")
+map("x", "<leader>p", "\"_dP")
+map("n", "<leader>d", "\"_dP")
+map("v", "<leader>d", "\"_dP")
 
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+y")
+map("n", "<leader>y", "\"+y")
+map("v", "<leader>y", "\"+y")
+map("n", "<leader>Y", "\"+y")
 
-vim.keymap.set("n", "<C-s>", ":w<CR>")
+map("n", "<C-s>", ":w<CR>")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+local opts = { noremap = true, silent = true }
+map("n", '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+map("n", '<A-.>', '<Cmd>BufferNext<CR>', opts)
+map("n", '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+map("n", '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+map("n", '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+map("n", '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+map("n", '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+map("n", '<A-c>', '<Cmd>BufferClose<CR>', opts)
