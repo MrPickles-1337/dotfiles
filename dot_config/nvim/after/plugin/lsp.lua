9dabc6e2641780eb82cebfebcd1175b7c10b1546
Kirill Popov <k.popov@hunterhelp.ru>
2023-03-24 09:52:44 +0300

    add type definition

--- dot_config/nvim/after/plugin/lsp.lua
+++ dot_config/nvim/after/plugin/lsp.lua
@@ -24,6 +24,7 @@ local my_on_attach = function(client, buffer)
     local opts = { buffer = buffer, remap = false }
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>td", function() vim.lsp.buf.type_definition() end, opts)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
     vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
     vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
