de0e471c90b9ba8e10158187391a1db1512e1330
Kirill Popov <k.popov@hunterhelp.ru>
2023-04-06 13:14:48 +0300

    add filename in lualine

--- dot_config/nvim/after/plugin/lualine.lua
+++ dot_config/nvim/after/plugin/lualine.lua
@@ -1,5 +1,11 @@
 require('lualine').setup {
     sections = {
+        lualine_b = {
+            {
+                'filename',
+                path = 1
+            }
+        },
         lualine_c = {
             'lsp_progress'
         }
