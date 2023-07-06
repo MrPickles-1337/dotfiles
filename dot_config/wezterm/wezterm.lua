8a5ca2cd8be5ad1b879373a62ebf9722e003f207
Kirill Popov <k.popov@hunterhelp.ru>
2023-07-06 15:43:34 +0300

    yep

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -10,5 +10,6 @@ config.font = wezterm.font 'Cascadia Code'
 config.window_background_opacity = 0.4
 config.enable_tab_bar = false
 config.set_environment_variables = { yep = "cock" }
+config.window_close_confirmation = 'NeverPrompt'
 
 return config
