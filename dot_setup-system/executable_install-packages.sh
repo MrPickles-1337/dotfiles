d494a611fc356f8e6487c74897138459141d6cf8
Kirill Popov <k.popov@hunterhelp.ru>
2023-07-24 17:32:40 +0300

    add setup scripts

---
+++ dot_setup-system/executable_install-packages.sh
@@ -0,0 +1,3 @@
+yay -S --needed hyprland kitty waybar-hyprland-git xdg-desktop-portal-hyprland \
+    polkit-gnome pavucontrol btop ttf-jetbrains-mono-nerd noto-fonts-emoji \
+    sddm-git dunst
---
+++ dot_setup-system/executable_yay.sh
@@ -0,0 +1,4 @@
+cd /tmp
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
