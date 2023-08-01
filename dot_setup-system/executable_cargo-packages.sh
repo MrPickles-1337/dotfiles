5d960fd86b5678fbcff58bcd56ed5cd19bfd1343
Kirill Popov <k.popov@hunterhelp.ru>
2023-08-01 17:23:10 +0300

    yep

--- dot_setup-system/executable_cargo-packages.sh
+++ dot_setup-system/executable_cargo-packages.sh
@@ -1,1 +1,1 @@
-cargo install exa bat cargo-aoc cargo-expand cargo-make cargo-outdated sccache cargo-info cargo-watch du-dust cargo-update cargo-xtask starship zellij ripgrep
+cargo install exa bat cargo-aoc cargo-expand cargo-make cargo-outdated sccache cargo-info cargo-watch du-dust cargo-update cargo-xtask starship zellij ripgrep loc
