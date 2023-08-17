616369d863c79486c01603b369885c436316cb05
Kirill Popov <k.popov@hunterhelp.ru>
2023-08-17 15:34:19 +0300

    add fd-find

--- dot_setup-system/executable_cargo-packages.sh
+++ dot_setup-system/executable_cargo-packages.sh
@@ -1,1 +1,1 @@
-cargo install exa bat cargo-aoc cargo-expand cargo-make cargo-outdated sccache cargo-info cargo-watch du-dust cargo-update cargo-xtask starship zellij ripgrep loc
+cargo install exa bat cargo-aoc cargo-expand cargo-make cargo-outdated sccache cargo-info cargo-watch du-dust cargo-update cargo-xtask starship zellij ripgrep fd-find
