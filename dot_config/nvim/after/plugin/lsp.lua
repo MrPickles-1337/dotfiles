18422d2ddb5ea9478e2c167b991552c991b6f209
MrPickles <kirill2013popov@gmail.com>
2025-07-17 02:09:34 +0300

    fix lsp config

--- dot_config/nvim/after/plugin/lsp.lua
+++ dot_config/nvim/after/plugin/lsp.lua
@@ -1,55 +1,59 @@
-local lsp = require('lsp-zero')
-local lspconfig = require("lspconfig")
-require("lsp-format").setup {}
+local lspconfig_defaults = require('lspconfig').util.default_config
+lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+    'force',
+    lspconfig_defaults.capabilities,
+    require('cmp_nvim_lsp').default_capabilities()
+)
 
-lsp.preset('recommended')
+vim.api.nvim_create_autocmd('LspAttach', {
+    desc = 'LSP actions',
+    callback = function(event)
+        local optes = {buffer = event.buf}
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>td", function() vim.lsp.buf.type_definition() end, opts)
+        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+        vim.keymap.set("n", "<leader>lca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<F2>", function() vim.lsp.buf.rename() end, opts)
+        -- vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format() end, opts)
+    end,
+})
 
 local cmp = require('cmp')
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<Tab>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-Space>'] = cmp.mapping.complete(),
+
+cmp.setup({
+    sources = {
+        {name = 'nvim_lsp'},
+    },
+    snippet = {
+        expand = function(args)
+            vim.snippet.expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+    }),
 })
 
 
-lsp.setup_nvim_cmp({
-    mapping = cmp_mappings
-})
-local my_on_attach = function(client, buffer)
-    require("lsp-format").on_attach(client)
-    local opts = { buffer = buffer, remap = false }
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>td", function() vim.lsp.buf.type_definition() end, opts)
-    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set("n", "<leader>lca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>lrn", function() vim.lsp.buf.rename() end, opts)
-    -- vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-    vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format() end, opts)
-end
-
-
--- require("neodev").setup {
---     library = { plugin = { "nvim-dap-ui" }, types = true }
--- }
-
-lsp.on_attach(my_on_attach)
-lspconfig.gopls.setup { on_attach = my_on_attach }
-
-lsp.nvim_workspace()
-lsp.setup()
+require('lspconfig').lua_ls.setup({})
+--require('lspconfig').rust_analyzer.setup({})
+require("lsp-format").setup {}
+require("pubspec-assist").setup()
 
 vim.diagnostic.config({
     virtual_text = true
 })
-
-require("pubspec-assist").setup()
 
 require("flutter-tools").setup {
     ui = {
